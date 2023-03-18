b49b91db861b84944b7f359c3387ce3db1e23218
Wazven <aldikuzkuz55@gmail.com>
2023-03-18 21:45:05 +0700

    menambahkan fungsi handler addBookHandler

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const routes = require("./routes");
 
 const init = async () => {
   const server = Hapi.server({
+    //Kriteria 1: Aplikasi menggunakan port 9000
     port: 9000,
     host: "localhost",
     routes: {
